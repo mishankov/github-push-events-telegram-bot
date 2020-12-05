cde2da4f9aeacfff0dd84fc06ce6bd62baf2b838
mishankov <mishankov@mail.com>
2020-12-05 18:25:58 +0300

    rich bot message

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from models.push_webhook_payload import PushWebhookPayload
 from telegram_bot.bot import Bot
 
-
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_USER_ID = os.getenv("TELEGRAM_USER_ID", "")
 
@@ -30,7 +29,30 @@ app.add_middleware(
 def receive_github_repository_webhook(payload: PushWebhookPayload):
     print([payload.__dict__])
 
-    bot.send_message(chat_id=TELEGRAM_USER_ID, parse_mode="MarkdownV2", text="Push from: *{}*".format(payload.pusher.name))
+    message = "New push in repository [{}]({})".format(
+        payload.repository.full_name, payload.repository.html_url
+    )
+    if payload.sender is not None:
+        message += " from [{}]({})".format(
+            payload.sender.login, payload.sender.html_url
+        )
+
+    if len(payload.commits) > 0:
+        message += "\n *Commits:*"
+        for commit in payload.commits:
+            message += "\n- _[{}]({})_".format(commit.id, commit.url)
+            message += "\n Message: {}".format(commit.message)
+
+            if len(commit.added) > 0:
+                message += "\n  _Added:_ {}".format(",".join(commit.added))
+
+            if len(commit.removed) > 0:
+                message += "\n  Removed:_ {}".format(",".join(commit.removed))
+
+            if len(commit.modified) > 0:
+                message += "\n  _Modified:_ {}".format(",".join(commit.modified))
+
+    bot.send_message(chat_id=TELEGRAM_USER_ID, parse_mode="MarkdownV2", text=message)
 
     return {"status": "OK"}
 
