f685070e161f09c7bdcc7593781f89fe22e4a623
mishankov <mishankov@mail.com>
2020-12-06 16:38:32 +0300

    fix escape_html

--- src/telegram/utils.py
+++ src/telegram/utils.py
@@ -1,2 +1,2 @@
 def escape_html(text: str) -> str:
-    return text.replace("<", "&lt").replace(">", "&gt").replace("&", "&amp")
+    return text.replace("&", "&amp").replace("<", "&lt").replace(">", "&gt")
